414547fbf46fa58b0946ea378cc81e76d532802c
jkusita <46511897+jkusita@users.noreply.github.com>
2019-08-07 09:14:11 +0800

    Initial code

--- rename-dates.py
+++ rename-dates.py
@@ -1,5 +1,14 @@
-#test comment for git
+# Searches for files in current directory and renaming the file's name containing dates in american-style into european-style.
 
-print("Hello git!")
-user_input = input("Please enter your name: ")
-print(f"This is {user_input}'s repository for a project from the Automate book! ")
+# rename-dates.py - Renames filenames with American MM-DD-YYYY date format to European DD-MM-YYYY.
+
+import shutil, os, re
+
+# Create a regex that matches files with the American date format.
+
+datePattern = re.compile(r"""^(.*?) # all text before the date
+    ((0|1)?\d)-                     # one or two digits for the month
+    ((0|1|2|3)?\d)-                 # one or two digits for the day
+    ((19|20)\d\d)                   # four digits for the year
+    (.*?)$                          # all text after the date
+     """, re.VERBOSE)
