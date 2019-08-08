8e1e6916b74d94dd85c44875d924e7a991ec13c8
jkusita <46511897+jkusita@users.noreply.github.com>
2019-08-08 09:45:31 +0800

    Made some progress

--- rename-dates.py
+++ rename-dates.py
@@ -12,3 +12,23 @@ datePattern = re.compile(r"""^(.*?) # all text before the date
     ((19|20)\d\d)                   # four digits for the year
     (.*?)$                          # all text after the date
      """, re.VERBOSE)
+# If the iniitial numbers are optional, what happens if you don't enter an initial number and it just proceeds to \d?
+
+# TODO: Loop over the files in the working directory.
+# TODO: Skip files without a date.
+# TODO: Get the different parts of the filename.
+# TODO: Form the European-style filename.
+# TODO: Get the full, absolute file paths.
+# TODO: Rename the files.
+
+
+
+
+# ctrl + tab to switch between recent tabs
+# always save before running, coderunner saves slow.
+
+
+# TODO: how does this work?: (0|1)?
+test_regex = re.compile(r"(0|1)?")
+mo = test_regex.search("0 burgers are not that good!")
+print(mo.group())
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+# print(mo.group(2))
