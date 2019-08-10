8e498fbfc560c18eae6278556f52a5f7dc279c49
jkusita <46511897+jkusita@users.noreply.github.com>
2019-08-10 23:14:45 +0800

    created a file to try and solve the project first before looking at the book's solution.

---
+++ solve-problem-first.py
@@ -0,0 +1,8 @@
+# My plan:
+
+# TODO: make a regex that matches "test-folder- morethanoneintegershere"
+# TODO: store all the file names in the folder in question in a list (first_list).
+# TODO: grab from that list the file names that matches the regex into a new list (second_list)
+# TODO: make another list (third_list) that has the new file names of the old files name where they have identical indicis.
+# TODO: Change the old folder names into the new ones through their indices.
+# TODO: replace the old folder names by getting their old filename from second_list and looking for the folder, once that folder is selected, change it with the new folder name, which is matched with the same index as the folder's name in the second_list, and replace it with the new folder name in the third_list
