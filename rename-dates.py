68d71f738cd1ec6ab477e4ed1f3b45ef44d6e365
jkusita <46511897+jkusita@users.noreply.github.com>
2019-08-21 14:11:55 +0800

    Changed code to the code from automate

--- rename-dates.py
+++ rename-dates.py
@@ -1,33 +1,25 @@
-# rename-dates.py - Renames filenames with American MM-DD-YYYY date format to European DD-MM-YYYY.
+   #! python3
+   # renameDates.py - Renames filenames with American MM-DD-YYYY date format
+   # to European DD-MM-YYYY.
 
 import shutil, os, re
 
-# Directory of the folder containing the files.
-directory_path = ("/Users/ramteechua/Desktop/rename-dates-folder")
-
-# Creates a regex that matches files with the American date format.
-date_pattern = re.compile(r"""
-    ^(.*?)                           # all text before the date
-    ((0|1)?\d)-                     # one or two digits for the month
-    ((0|1|2|3)?\d)-                 # one or two digits for the day
-    ((19|20)\d\d)                   # four digits for the year
-    (.*?)$                          # all text after the date
+   # Create a regex that matches files with the American date format.
+datePattern = re.compile(r"""^(.*?) # all text before the date
+       ((0|1)?\d)-                     # one or two digits for the month
+       ((0|1|2|3)?\d)-                 # one or two digits for the day
+       ((19|20)\d\d)                   # four digits for the year
+       (.*?)$                          # all text after the date
      """, re.VERBOSE)
 
-os.chdir(directory_path)
+   # TODO: Loop over the files in the working directory.
 
-for file in os.listdir(directory_path):
-    mo = date_pattern.search(file)
-    if mo != None:
-      # Arranged to (text)DD/MM/YY(text)
-      new_date = mo.group(1) + mo.group(4) + "-" + mo.group(2) + "-" + mo.group(6) + mo.group(8)
-      shutil.move(file, new_date ) 
+   # TODO: Skip files without a date.
 
+   # TODO: Get the different parts of the filename.
 
-# George Hotz: "Yeah, that looks fun."
-# Keyboard shortcuts: 
-# ctrl + g (line number)        
-# ctrl + tab to switch between recent tabs
-# always save before running, coderunner saves slow.
+   # TODO: Form the European-style filename.
 
-# TODO: try to do the problem yourself first before looking at the solution.
+   # TODO: Get the full, absolute file paths.
+
+   # TODO: Rename the files.
